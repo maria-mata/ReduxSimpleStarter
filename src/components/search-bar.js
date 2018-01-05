bbfd8638ba72d11af12fdd08a23478cd27e1d83f
Maria Mata <mariajo.mata@gmail.com>
2018-01-05 14:38:28 -0700

    add search change feature

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -4,19 +4,23 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = { term: ''}
-  }
+  };
 
   render() {
     return (
       <div className="search-bar">
         <input
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value })} />
+          onChange={event => this.onInputChange(event.target.value)} />
       </div>
-
     );
-  }
-}
+  };
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  };
+};
 
 
-export default SearchBar
+export default SearchBar;
